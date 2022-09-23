605a6db3c39f700c4d99ff63c6a844e5821290c4
josue <josue@LAPTOP-I3VRBKPN>
2022-09-23 09:25:28 -0600

    Agregar archivos de proyecto.

---
+++ Division de Propiedades/Division.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_de_Propiedades
+{
+    internal class Division
+    {
+        double v1, v2;
+
+        public double V1 { get => v1; set => v1 = value; }
+        public double V2 { get => v2; set => v2 = value; }
+
+        public double Dividir()
+        {
+            return V1 / V2;
+        }
+
+    }
+
+}
